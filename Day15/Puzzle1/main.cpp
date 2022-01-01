f0408256f7633b092b212c79c05a60dff168e77d
gjconwell <gjconwell@gmail.com>
2022-01-01 15:48:49 -0800

    Finished Day 15 Puzzle 1

--- Day15/Puzzle1/main.cpp
+++ Day15/Puzzle1/main.cpp
@@ -2,58 +2,71 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <cmath>
+#include <algorithm>
+#include <queue>
 #include <map>
-#include <list>
 
 //cd /mnt/c/Users/conweg/Documents/Coding/Advent_of_Code_2021/Day15/Puzzle1
 //g++ main.cpp -o main.exe -Wall
 //./main.exe
-//695
 
-int path(std::vector<std::vector<int>> &map,
-         int i, int j, int risk, int &min){
-  risk += map[i][j]; 
-  if (risk >= min) {
-    // std::cout << "terminate" << '\n';
-    return min;
+typedef std::pair<int, std::pair<int, int>> PQElement;
+
+int Retrace(std::map<std::pair<int, int>, std::pair<int, int>> &came_from, 
+            std::vector<std::vector<int>> &map) {
+  int risk = 0;
+  std::map<std::pair<int, int>, std::pair<int, int>>::iterator it = came_from.find(std::make_pair(map.size() - 1,map.size() - 1));
+  while (it->second.first != 0 || it->second.second != 0) {
+    risk += map[it->first.first][it->first.second];
+    it = came_from.find(std::make_pair(it->second.first,it->second.second));
   }
-  // mem.push_back(std::make_pair(i, j));
-  if (j + 1 == (int) map.size() && i + 1 == (int) map.size()) {
-    // for (int k = 0; k < (int) mem.size(); k++) {
-    //   std::cout << mem[k].first << "," << mem[k].second << ' ';
-    // }
-    // std::cout << '\n';
-    if (risk < min) {
-      min = risk;
-    }
-    std::cout << "finished " << risk << '\n';
-    return min;
-  }
-  if (i + 1 < (int) map.size() && j + 1 < (int) map[0].size()) {
-    // std::cout << map[i + 1][j] << " " <<  map[i][j + 1] << '\n';
-    path(map, i + 1, j, risk, min);
-    path(map, i, j + 1, risk, min);
-  } else if (j + 1 < (int) map[0].size()) {
-    path(map, i, j + 1, risk, min);
-  } else if (i + 1 < (int) map.size()) {
-    path(map, i + 1, j, risk, min);
-  }
-  return min;
+  risk += map[it->first.first][it->first.second];
+  return risk;
 }
 
-int borderline(std::vector<std::vector<int>> &map) { 
-  int i = 0;
-  int j = 0;
-  int risk = -map[0][0];
-  while (i + 1 < (int) map.size() || j + 1 < (int) map[0].size()) {
-    risk += map[i][j];
-    if (j + 1 < (int) map[0].size()) {
-      j++;
-    } else if (i + 1 < (int) map.size()) {
-      i++;
+void Neighbor(std::vector<std::vector<int>> &map, int x, int y, 
+              std::pair<int, int> &current, std::priority_queue<PQElement, 
+              std::vector<PQElement>, std::greater<PQElement>> &frontier, 
+              std::map<std::pair<int, int>, std::pair<int, int>> &came_from,
+              std::map<std::pair<int, int>, int> &cost_so_far) {
+  bool bounds = false;
+  if (x != 0 && current.first + x < (int) map.size() && current.first + x > 0) {
+    bounds = true;
+  } else if (y != 0 && current.second + y < (int) map.size() && current.second + y > 0) {
+    bounds = true;
+  }
+  if (bounds) {
+    std::map<std::pair<int, int>, int>::iterator it = cost_so_far.find(std::make_pair(current.first, current.second));
+    int new_cost = it->second + map[current.first + x][current.second + y];
+    it = cost_so_far.find(std::make_pair(current.first + x, current.second + y));
+    if (it == cost_so_far.end()) {
+      cost_so_far[std::make_pair(current.first + x,current.second + y)] = new_cost;
+      frontier.push(std::make_pair(new_cost, 
+                    std::make_pair(current.first + x, current.second + y)));
+      came_from[std::make_pair(current.first + x, current.second + y)] = std::make_pair(current.first,current.second);
     }
   }
-  return risk;
+}
+
+int Astar(std::vector<std::vector<int>> &map) { 
+  std::priority_queue<PQElement, std::vector<PQElement>, std::greater<PQElement>> frontier;
+  std::map<std::pair<int, int>, std::pair<int, int>> came_from;
+  std::map<std::pair<int, int>, int> cost_so_far;
+  frontier.push(std::make_pair(map[0][0], std::make_pair(0,0)));
+  cost_so_far[std::make_pair(0,0)] = map[0][0];
+  while (!frontier.empty()) {
+    std::pair<int, int> current = frontier.top().second;
+    frontier.pop();
+    if (current.first == (int) map.size() - 1 && current.second == (int) map.size() - 1) {
+      break;
+    }
+    Neighbor(map, 1, 0, current, frontier, came_from, cost_so_far);
+    Neighbor(map, 0, 1, current, frontier, came_from, cost_so_far);
+    Neighbor(map, -1, 0, current, frontier, came_from, cost_so_far);
+    Neighbor(map, 0, -1, current, frontier, came_from, cost_so_far);
+  }
+  return Retrace(came_from, map);
 }
 
 int main(int argc, char const *argv[]) {
@@ -67,10 +80,6 @@ int main(int argc, char const *argv[]) {
     }
     map.push_back(temp);
   }
-  // std::vector<std::pair<int, int>> mem;
-  int min = borderline(map);
-  std::cout << min << '\n';
-  
-  std::cout << path(map, 0, 0, -map[0][0], min) - map[0][0]<< '\n';
+  std::cout << Astar(map) << '\n';
   return 0;
 }
