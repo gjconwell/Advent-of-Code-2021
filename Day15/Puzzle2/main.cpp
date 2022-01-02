054a8a48db6c34561bd854063697a055c0e51408
gjconwell <gjconwell@gmail.com>
2022-01-01 16:34:37 -0800

    Finished Day 15 Puzzle 2

--- Day15/Puzzle2/main.cpp
+++ Day15/Puzzle2/main.cpp
@@ -2,79 +2,114 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <queue>
 #include <map>
-#include <list>
 
-//cd /mnt/c/Users/conweg/Documents/Coding/Advent_of_Code_2021/Day14/Puzzle2
+//cd /mnt/c/Users/conweg/Documents/Coding/Advent_of_Code_2021/Day15/Puzzle2
 //g++ main.cpp -o main.exe -Wall
 //./main.exe
 
-long int Step(std::map<std::string, long int> &polymer, std::map<char,long int> &chars,
-         std::vector<std::pair<std::string, char>> &instruct) {
-  std::map<std::string, long int> temp;
-  std::map<std::string, long int>::iterator it = polymer.begin();
-  while (it != polymer.end()) {
-    for (int i = 0; i < (int) instruct.size(); i++) {
-      if (instruct[i].first == it->first) {
-        std::string pair1 = "";
-        pair1 += it->first[0];
-        pair1 += instruct[i].second;
-        std::string pair2 = "";
-        pair2 += instruct[i].second;
-        pair2 += it->first[1];
-        temp[pair1] += it->second;
-        temp[pair2] += it->second;
-        chars[instruct[i].second] += it->second;
-        break;
-      }
-    }
-    it++;
+typedef std::pair<int, std::pair<int, int>> PQElement;
+
+int Retrace(std::map<std::pair<int, int>, std::pair<int, int>> &came_from, 
+            std::vector<std::vector<int>> &map) {
+  int risk = 0;
+  std::map<std::pair<int, int>, std::pair<int, int>>::iterator it = came_from.find(std::make_pair(map.size() - 1,map.size() - 1));
+  while (it->second.first != 0 || it->second.second != 0) {
+    risk += map[it->first.first][it->first.second];
+    it = came_from.find(std::make_pair(it->second.first,it->second.second));
   }
-  polymer = temp;
-  long int max = 0;
-  std::map<char, long int>::iterator it2 = chars.begin();
-  long int min = it2->second;
-  while (it2 != chars.end()) {
-    if (max < it2->second) {
-      max = it2->second;
-    }
-    if (min > it2->second) {
-      min = it2->second;
-    }
-    it2++;
+  risk += map[it->first.first][it->first.second];
+  return risk;
+}
+
+void Neighbor(std::vector<std::vector<int>> &map, int x, int y, 
+              std::pair<int, int> &current, std::priority_queue<PQElement, 
+              std::vector<PQElement>, std::greater<PQElement>> &frontier, 
+              std::map<std::pair<int, int>, std::pair<int, int>> &came_from,
+              std::map<std::pair<int, int>, int> &cost_so_far) {
+  bool bounds = false;
+  if (x != 0 && current.first + x < (int) map.size() && current.first + x > 0) {
+    bounds = true;
+  } else if (y != 0 && current.second + y < (int) map.size() && current.second + y > 0) {
+    bounds = true;
   }
-  return max - min;
+  if (bounds) {
+    std::map<std::pair<int, int>, int>::iterator it = cost_so_far.find(std::make_pair(current.first, current.second));
+    int new_cost = it->second + map[current.first + x][current.second + y];
+    it = cost_so_far.find(std::make_pair(current.first + x, current.second + y));
+    if (it == cost_so_far.end()) {
+      cost_so_far[std::make_pair(current.first + x,current.second + y)] = new_cost;
+      frontier.push(std::make_pair(new_cost, 
+                    std::make_pair(current.first + x, current.second + y)));
+      came_from[std::make_pair(current.first + x, current.second + y)] = std::make_pair(current.first,current.second);
+    }
+  }
+}
+
+int Astar(std::vector<std::vector<int>> &map) { 
+  std::priority_queue<PQElement, std::vector<PQElement>, std::greater<PQElement>> frontier;
+  std::map<std::pair<int, int>, std::pair<int, int>> came_from;
+  std::map<std::pair<int, int>, int> cost_so_far;
+  frontier.push(std::make_pair(map[0][0], std::make_pair(0,0)));
+  cost_so_far[std::make_pair(0,0)] = map[0][0];
+  while (!frontier.empty()) {
+    std::pair<int, int> current = frontier.top().second;
+    frontier.pop();
+    if (current.first == (int) map.size() - 1 && current.second == (int) map.size() - 1) {
+      break;
+    }
+    Neighbor(map, 1, 0, current, frontier, came_from, cost_so_far);
+    Neighbor(map, 0, 1, current, frontier, came_from, cost_so_far);
+    Neighbor(map, -1, 0, current, frontier, came_from, cost_so_far);
+    Neighbor(map, 0, -1, current, frontier, came_from, cost_so_far);
+  }
+  return Retrace(came_from, map);
 }
 
 int main(int argc, char const *argv[]) {
+  std::vector<std::vector<int>> map;
   std::ifstream istr("input.txt");
   std::string info;
-  std::map<char,long int> chars;
-  std::map<std::string, long int> polymer;
-  std::vector<std::pair<std::string, char>> instruct;
-  int iter = 0;
   while (istr >> info) {
-    if (iter == 0) {
-      for (int i = 0; i < (int) info.size() - 1; i++) {
-        chars[info[i]]++;
-        std::string num = "";
-        num += info[i];
-        num += info[i + 1];
-        polymer[num]++;
-      }
-      chars[info[info.size() - 1]]++;
-    } else {
-      std::string temp = info;
-      istr >> info;
-      istr >> info;
-      instruct.push_back(std::make_pair(temp, info[0]));
+    std::vector<int> temp;
+    for (int i = 0; i < (signed) info.size(); i++) {
+      temp.push_back(info[i] - '0');
     }
-    iter++;
+    map.push_back(temp);
   }
-  long int output = 0;
-  for (int i = 0; i < 40; i++) {
-    output = Step(polymer, chars, instruct);
+  int orig = map.size();
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < (int) map.size(); j++) {
+      std::vector<int> temp;
+      for (int k = (int) map[j].size() - orig; k < (int) map[j].size(); k++) {
+        int num;
+        if (map[j][k] + 1 > 9) {
+          num = 1;
+        } else {
+          num = map[j][k] + 1;
+        }
+        temp.push_back(num);
+      }
+      map[j].insert(map[j].end(), temp.begin(), temp.end());
+    }
   }
-  std::cout << output << '\n';
+  for (int i = 0; i < 4; i++) {
+    int orig2  = map.size();
+    for (int j = orig2 - orig; j < orig2; j++) {
+      std::vector<int> temp2;
+      for (int k = 0; k < (int) map[j].size(); k++) {
+        int num;
+        if (map[j][k] + 1 > 9) {
+          num = 1;
+        } else {
+          num = map[j][k] + 1;
+        }
+        temp2.push_back(num);
+      }
+      map.push_back(temp2);
+    }
+  }
+  std::cout << Astar(map) << '\n';
   return 0;
 }
